ff883ab2cac4aef63e6189743d96d05a0c9022bc
guptaryan73 <guptaryan73@gmail.com>
2024-08-04 23:39:29 +0530

    added

---
+++ dsa/49/49.01.cpp
@@ -0,0 +1,42 @@
+/* https://leetcode.com/problems/merge-two-sorted-lists/ */
+
+void solve(Node<int> *first, Node<int> *second)
+{
+
+    Node *curr1 = first;
+    Node *next1 = curr1->next;
+
+    Node *curr2 = second;
+    Node *next2 = curr2->next;
+
+    while (next1 != NULL && curr2 != NULL)
+    {
+
+        if ((curr2->data >= curr1->data) && (curr2->data <= next1->data))
+        {
+
+            curr1->next = curr2;
+            curr2->next = next1;
+            curr1 = curr2;
+            curr2 = next2;
+        }
+    }
+}
+
+Node<int> *sortTwoLists(Node<int> *first, Node<int> *second)
+{
+    if (first == NULL)
+        return second;
+
+    if (second == NULL)
+        return first;
+
+    if (first->data <= second->data)
+    {
+        solve(first,second);
+    }
+    else
+    {
+        solve(second, first);
+    }
+}
