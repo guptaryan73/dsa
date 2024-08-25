ff78773fea52363180fe6c58afe3e9d55654fbb0
guptaryan73 <guptaryan73@gmail.com>
2024-08-25 22:49:38 +0530

    added files

---
+++ dsa/53/53.02.cpp
@@ -0,0 +1,19 @@
+/* https://www.naukri.com/code360/problems/flatten-a-linked-list_1112655*/
+
+/*
+ * Definition for linked list.
+ * class Node {
+ *  public:
+ *		int data;
+ *		Node *next;
+ * 		Node *child;
+ *		Node() : data(0), next(nullptr), child(nullptr){};
+ *		Node(int x) : data(x), next(nullptr), child(nullptr) {}
+ *		Node(int x, Node *next, Node *child) : data(x), next(next), child(child) {}
+ * };
+ */
+
+Node* flattenLinkedList(Node* head) 
+{
+    
+}
